259d18f9194968a08146a4435debea0ffea339ef
IlhamOZI <ilhamfahrozi0305@gmail.com>
2025-12-26 13:01:29 +0700

    Add mandatory authentication: fullscreen auth modal blocks all access until login/register

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/lib/AuthContext'
+import AuthModal from '@/lib/AuthModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <AuthModal />
       <Component {...pageProps} />
     </AuthProvider>
   )
